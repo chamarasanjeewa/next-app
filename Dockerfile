534b7b000a0d75cce2eacf96dfaac14d10506ad7
chamara <chamara@chamaras-MacBook-Pro.local>
2023-04-04 20:15:42 +0530

    new files

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
-FROM node:16-alpine
-
-WORKDIR /frontend
-
+FROM node:lts-alpine3.13 as builder
+ARG NODE_ENV
+ENV NODE_ENV=$NODE_ENV
+WORKDIR '/app'
 COPY package*.json ./
-
 RUN npm install
-
 COPY . .
-
-EXPOSE 4000
-
-CMD npm run dev
+RUN npm run build
+CMD ["npm", "start"]
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,39 @@
-"use client";
+// "use client";
 
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 
 export default function Home() {
+  const a = process.env.testEnvironmentVar;
+  const b = process.env.testEnvFromDocker;
+  console.log("environment variable .....", a);
+  console.log("environment variable from docker.....", b);
+  console.log(
+    "environment exported from bash.....",
+    process.env.exportedFromBash
+  );
   return (
-    <Grid container height="calc(100vh - 38px)" alignItems="center" justifyContent="center" direction="column">
-      <h1 className="text-blue-600">Using Material UI with Next.js 13</h1>
-      <h4 className="text-red-500">(with Tailwind CSS)</h4>{" "}
-      <Stack direction="row" columnGap={1}>
-        <Button variant="text" className="text-red-500 bg-gray-200">
-          Text
-        </Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-    </Grid>
+    <>
+      <div>env from env file {a}</div>
+      <div>env from docker {b}</div>
+    </>
+    // <Grid
+    //   container
+    //   height="calc(100vh - 38px)"
+    //   alignItems="center"
+    //   justifyContent="center"
+    //   direction="column"
+    // >
+    //   <h1 className="text-blue-600">Using Material UI with Next.js 13</h1>
+    //   <h4 className="text-red-500">(with Tailwind CSS)</h4>{" "}
+    //   <Stack direction="row" columnGap={1}>
+    //     <Button variant="text" className="text-red-500 bg-gray-200">
+    //       Text
+    //     </Button>
+    //     <Button variant="contained">Contained</Button>
+    //     <Button variant="outlined">Outlined</Button>
+    //   </Stack>
+    // </Grid>
   );
 }
