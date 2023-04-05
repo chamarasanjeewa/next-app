421891889d3be07de7726732e1a61be2d07fc206
Chamara sanjeewa <chamara@chamaras-MacBook-Pro.local>
2023-04-05 18:35:20 +0530

    fixed

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 echo \$MY_VAR
 export exportedFromBash=MY_VAR
 echo "The value of BAZ is after: $1 $2"
-docker-compose -f docker-compose.yml up --build -d
+docker compose -f docker-compose.yml up --build -d
