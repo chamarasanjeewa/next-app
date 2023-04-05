95e7d2e0cb3033de1cd2dcf4036e4cb5b3f46575
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2023-04-05 21:02:38 +0530

    minor

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@
 echo \$MY_VAR
 export exportedFromBash=MY_VAR
 echo "The value of BAZ is after: $1 $2"
+echo "docker version...."
+docker-compose --version
+echo "..................."
 docker-compose -f docker-compose.yml up --build -d
