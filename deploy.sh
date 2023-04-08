0b620a8587202b460e70866f25ae01b2474b9db8
Chamara Sanjeewa <chamara.sanjeewa@gmail.com>
2023-04-08 19:35:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo \$MY_VAR
 export exportedFromBash=MY_VAR
-echo "The value of BAZ is after: $1 $2"
-echo "docker version...."
-docker-compose --version
+# echo "docker version...."
+# docker-compose --version
 echo "...................$ENV"
  echo ".....The value of MY_VAR is: $ENVFILE"
-# docker-compose -f docker-compose.yml up --build -d
+docker-compose -f docker-compose.yml --env-file .\$ENVFILE up --build -d
