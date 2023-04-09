7ab4b67d05f1c4b6ea61b44bb2e4e10b35f49685
Chamara Sanjeewa <chamara.sanjeewa@gmail.com>
2023-04-09 08:38:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ export exportedFromBash=MY_VAR
 # echo "docker version...."
 # docker-compose --version
 echo "...................$ENV"
- echo ".....environment variables from file: $ENVFILE"
+ echo ".....environment variables from file: $FILE_CREDS"
 #docker-compose -f docker-compose.yml --env-file .\$ENVFILE up --build -d
