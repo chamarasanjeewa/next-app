e324c45bcf045c9275d38d38450b00d445b779f3
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2023-04-14 13:57:19 +0530

    feat: Add new feature to the app

--- pages/api/usrs.ts
+++ pages/api/usrs.ts
@@ -6,7 +6,7 @@ dotenv.config();
 console.log("analyxe vommtisfsdfsdf sdfdfafasfasf ");
 const client = new MongoClient(process.env.MONGODB_URI!, {
   //useNewUrlParser: true,
-  // useUnifiedTopology: true     dddd,
+  // useUnifiedTopology: true         dddd,
 });
 
 async function connectToDatabase(): Promise<Db> {
