b40f6effcb19779dcab34ce207e3529c223cf4ac
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2023-04-14 10:52:18 +0530

    minor

--- pages/api/usrs.ts
+++ pages/api/usrs.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const client = new MongoClient(process.env.MONGODB_URI!, {
   //useNewUrlParser: true,
-  // useUnifiedTopology: true   ,
+  // useUnifiedTopology: true     ,
 });
 
 async function connectToDatabase(): Promise<Db> {
