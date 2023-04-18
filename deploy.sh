7bd3b4f4785625c05533bbce88d92fc553d1783a
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2023-04-18 19:43:31 +0530

    fixd ddsf

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ echo \$MY_VAR
 export exportedFromBash=MY_VAR
 # echo "docker version...."
 # docker-compose --version
-echo "...................$ENV"
+echo ".......building from............$ENV"
  echo ".....environment variables from file: $FILE_CREDS"
 #docker-compose -f docker-compose.yml --env-file .\$ENVFILE up --build -d
